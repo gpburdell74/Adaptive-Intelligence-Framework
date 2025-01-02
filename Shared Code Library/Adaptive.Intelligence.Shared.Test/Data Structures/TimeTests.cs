4b5d3c2719e1f9ee42ba75fcdd200fc84b5ef8f4
Sam Jones <sjones@easyvotesolutions.com>
2025-01-02 14:11:06 -0500

    First set of additions.

---
+++ Shared Code Library/Adaptive.Intelligence.Shared.Test/Data Structures/TimeTests.cs
@@ -0,0 +1,219 @@
+﻿namespace Adaptive.Intelligence.Shared.Test.Data_Structures
+{
+    public class TimeTests
+    {
+        [Fact]
+        public void ConstructorTests()
+        {
+            Time? t = new Time();
+            Assert.NotNull(t);
+            Assert.Equal(0, t.Value.TotalSeconds);
+            Assert.Equal(0, t.Value.Second);
+            Assert.Equal(0, t.Value.Minute);
+            Assert.Equal(0, t.Value.Hour);
+
+            t = new Time(50);
+            Assert.NotNull(t);
+            Assert.Equal(50, t.Value.TotalSeconds);
+            Assert.Equal(50, t.Value.Second);
+            Assert.Equal(0, t.Value.Minute);
+            Assert.Equal(0, t.Value.Hour);
+
+            t = new Time(-9700);
+            Assert.NotNull(t);
+            Assert.Equal(0, t.Value.TotalSeconds);
+            Assert.Equal(0, t.Value.Second);
+            Assert.Equal(0, t.Value.Minute);
+            Assert.Equal(0, t.Value.Hour);
+
+            t = new Time(Int32.MaxValue);
+            Assert.NotNull(t);
+            Assert.Equal(86399, t.Value.TotalSeconds);
+            Assert.Equal(59, t.Value.Second);
+            Assert.Equal(59, t.Value.Minute);
+            Assert.Equal(23, t.Value.Hour);
+
+        }
+
+        [Fact]
+
+        public void Constructor_TotalSeconds_Valid()
+        {
+            var time = new Time(3600);
+            Assert.Equal(1, time.Hour);
+            Assert.Equal(0, time.Minute);
+            Assert.Equal(0, time.Second);
+        }
+
+        [Fact]
+        public void Constructor_HourMinute_Valid()
+        {
+            var time = new Time(1, 30);
+            Assert.Equal(1, time.Hour);
+            Assert.Equal(30, time.Minute);
+            Assert.Equal(0, time.Second);
+        }
+
+        [Fact]
+        public void Constructor_HourMinuteSecond_Valid()
+        {
+            var time = new Time(1, 30, 45);
+            Assert.Equal(1, time.Hour);
+            Assert.Equal(30, time.Minute);
+            Assert.Equal(45, time.Second);
+        }
+
+        [Fact]
+        public void Property_Hour_SetValid()
+        {
+            var time = new Time(0);
+            time.Hour = 5;
+            Assert.Equal(5, time.Hour);
+        }
+
+        [Fact]
+        public void Property_Minute_SetValid()
+        {
+            var time = new Time(0);
+            time.Minute = 45;
+            Assert.Equal(45, time.Minute);
+        }
+
+        [Fact]
+        public void Property_Second_SetValid()
+        {
+            var time = new Time(0);
+            time.Second = 30;
+            Assert.Equal(30, time.Second);
+        }
+
+        [Fact]
+        public void Method_AddHours_Valid()
+        {
+            var time = new Time(1, 0, 0);
+            var newTime = time.AddHours(2);
+            Assert.Equal(3, newTime.Hour);
+        }
+
+        [Fact]
+        public void Method_AddMinutes_Valid()
+        {
+            var time = new Time(1, 0, 0);
+            var newTime = time.AddMinutes(30);
+            Assert.Equal(1, newTime.Hour);
+            Assert.Equal(30, newTime.Minute);
+        }
+
+        [Fact]
+        public void Method_AddSeconds_Valid()
+        {
+            var time = new Time(1, 0, 0);
+            var newTime = time.AddSeconds(45);
+            Assert.Equal(1, newTime.Hour);
+            Assert.Equal(0, newTime.Minute);
+            Assert.Equal(45, newTime.Second);
+        }
+
+        [Fact]
+        public void Method_ToString_Valid()
+        {
+            var time = new Time(13, 30, 0);
+            Assert.Equal("1:30 PM", time.ToString());
+        }
+
+        [Fact]
+        public void Method_ToString_WithAmPm()
+        {
+            var time = new Time(1, 30, 0);
+            Assert.Equal("1:30:00", time.ToString(false));
+        }
+
+        [Fact]
+        public void Method_Parse_Valid()
+        {
+            var time = Time.Parse("13:30:00");
+            Assert.Equal(13, time.Hour);
+            Assert.Equal(30, time.Minute);
+            Assert.Equal(0, time.Second);
+        }
+
+        [Fact]
+        public void Method_TryParse_Valid()
+        {
+            bool success = Time.TryParse("13:30:00", out Time time);
+            Assert.True(success);
+            Assert.Equal(13, time.Hour);
+            Assert.Equal(30, time.Minute);
+            Assert.Equal(0, time.Second);
+        }
+
+        [Fact]
+        public void Method_Equals_Valid()
+        {
+            var time1 = new Time(1, 30, 0);
+            var time2 = new Time(1, 30, 0);
+            Assert.True(time1.Equals(time2));
+        }
+
+        [Fact]
+        public void Method_CompareTo_Valid()
+        {
+            var time1 = new Time(1, 30, 0);
+            var time2 = new Time(2, 0, 0);
+            Assert.True(time1.CompareTo(time2) < 0);
+        }
+
+        [Fact]
+        public void MinValueTest()
+        {
+            Time? instance = Time.MinValue;
+
+            Assert.NotNull(instance);
+            Assert.Equal(0, instance.Value.Hour);
+            Assert.Equal(0, instance.Value.Minute);
+            Assert.Equal(0, instance.Value.Second);
+            Assert.Equal(0, instance.Value.TotalSeconds);
+        }
+        [Fact]
+        public void MaxValueTest()
+        {
+            Time? instance = Time.MaxValue;
+
+            Assert.NotNull(instance);
+            Assert.Equal(23, instance.Value.Hour);
+            Assert.Equal(59, instance.Value.Minute);
+            Assert.Equal(59, instance.Value.Second);
+            Assert.Equal(86399, instance.Value.TotalSeconds);
+        }
+        [Fact]
+        public void NowTest()
+        {
+            Time? instance = Time.Now;
+            DateTime dt = DateTime.Now;
+
+            Assert.NotNull(instance);
+            Assert.Equal(dt.Hour, instance.Value.Hour);
+            Assert.Equal(dt.Minute, instance.Value.Minute);
+            Assert.Equal(dt.Second, instance.Value.Second);
+        }
+
+        [Fact]
+        public void CompareToInvalidObjectTest()
+        {
+            Time t = Time.Now;
+            DateTime dt = DateTime.Now;
+
+            bool isGood = false;
+            try
+            {
+                t.CompareTo(dt);
+            }
+            catch (ArgumentException ex)
+            {
+                isGood = true;
+            }
+
+            Assert.True(isGood);
+        }
+    }
+}
--- Shared Code Library/Adaptive.Intelligence.Shared/Data Type Extensions/StringExtensions.cs
+++ Shared Code Library/Adaptive.Intelligence.Shared/Data Type Extensions/StringExtensions.cs
@@ -1,236 +1,271 @@
-﻿using Adaptive.Intelligence.Shared.Logging;
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace Adaptive.Intelligence.Shared
 {
-	/// <summary>
-	/// Provides extension methods for string values.
-	/// </summary>
-	public static class StringExtensions
-	{
-		#region Private Member Declarations
-		/// <summary>
-		/// The dollar sign as a string.
-		/// </summary>
-		private const string DollarSign = "$";
-		/// <summary>
-		/// The plural ending for singular words that end in Y.
-		/// </summary>
-		private const string EndingsY = "ies";
-		/// <summary>
-		/// The plural ending for some words.
-		/// </summary>
-		private const string EndingsES = "es";
-		/// <summary>
-		/// The plural ending for other words.
-		/// </summary>
-		private const string EndingsS = "s";
-		/// <summary>
-		/// The singular ending for words that end in "SS".
-		/// </summary>
-		private const string EndingsSS = "ss";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionAles = "ales";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionIles = "iles";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionUles = "ules";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionRoutes = "routes";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionTypes = "types";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionPages = "pages";
-		/// <summary>
-		/// The endings exception strings.
-		/// </summary>
-		private const string EndingsExceptionPlates = "plates";
-		#endregion
+    /// <summary>
+    /// Provides extension methods for string values.
+    /// </summary>
+    public static class StringExtensions
+    {
+        #region Private Member Declarations
+        /// <summary>
+        /// The dollar sign as a string.
+        /// </summary>
+        private const string DollarSign = "$";
+        /// <summary>
+        /// The plural ending for singular words that end in Y.
+        /// </summary>
+        private const string EndingsY = "ies";
+        /// <summary>
+        /// The plural ending for some words.
+        /// </summary>
+        private const string EndingsES = "es";
+        /// <summary>
+        /// The plural ending for other words.
+        /// </summary>
+        private const string EndingsS = "s";
+        /// <summary>
+        /// The singular ending for words that end in "SS".
+        /// </summary>
+        private const string EndingsSS = "ss";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionAles = "ales";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionIles = "iles";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionUles = "ules";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionRoutes = "routes";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionTypes = "types";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionPages = "pages";
+        /// <summary>
+        /// The endings exception strings.
+        /// </summary>
+        private const string EndingsExceptionPlates = "plates";
+        #endregion
 
-		#region Public Static String Extension Methods / Functions
-		/// <summary>
-		/// Provides a string extension method for removing US dollar signs from
-		/// the string instance.
-		/// </summary>
-		/// <param name="s">
-		/// The string being extended.
+        #region Public Static String Extension Methods / Functions
+        /// <summary>
+        /// Provides a string extension method for removing US dollar signs from
+        /// the string instance.
+        /// </summary>
+        /// <param name="s">
+        /// The string being extended.
+        /// </param>
+        /// <returns>
+        /// The modified string value.
+        /// </returns>
+        public static string CleanUpDollarText(this string s)
+        {
+            return s.Replace(DollarSign, string.Empty);
+        }
+        /// <summary>
+        /// Finds the first non-numeric character in the string.
+        /// </summary>
+        /// <param name="original">
+        /// The string instance.
+        /// </param>
+        /// <param name="isFloatingPoint">
+        /// A value indicating whether to allow for a floating-point number.
+        /// </param>
+        /// <returns>
+        /// An integer indicating the first position of a non-numeric character in the string, or 
+        /// -1 if there are no characters to examine or a non-numeric character could not be found.
+        /// </returns>
+        public static int FindFirstNonNumericCharacter(this string original, bool isFloatingPoint)
+        {
+            // Allow one period for floating point numbers.
+            bool dotFound = !isFloatingPoint;
+            int index = 0;
+            int position = -1;
+            int length = original.Length;
+
+            while (index < length && position == -1)
+            {
+                char charToExamine = original[index];
+
+                // If the character is not a digit...
+                if (!char.IsDigit(charToExamine))
+                {
+                    // If we have not yet encountered a period, treat the period as a number -
+                    // unless <i>isFloatingPoint</i> is <b>false</b>.
+                    if (charToExamine == '.' && !dotFound)
+                        dotFound = true;
+                    else
+                        position = index;
+                }
+                index++;
+            };
+
+            return position;
+        }
+        /// <summary>
+        /// Modifies the plural word back to its singular form.
+        /// </summary>
+        /// <param name="originalValue">The original value to be modified.</param>
+        /// <returns>
+        /// The English singular form of the provided word.
+        /// </returns>
+        public static string Singularize(this string originalValue)
+        {
+            string returnValue = originalValue;
+
+            if (!string.IsNullOrEmpty(originalValue))
+            {
+                // Create the comparison string.
+                string comparisonValue = originalValue.ToLower().Trim();
+                originalValue = originalValue.Trim();
+
+                if (comparisonValue.EndsWith(EndingsY))
+                {
+                    // Ends with "ies".
+                    returnValue = originalValue.Substring(0, comparisonValue.Length - 3) + "y";
+                }
+                else if (comparisonValue.EndsWith(EndingsES))
+                {
+                    // Exclude the known exceptions.
+                    if (comparisonValue.EndsWith(EndingsExceptionTypes) ||
+                        comparisonValue.EndsWith(EndingsExceptionPages) ||
+                        comparisonValue.EndsWith(EndingsExceptionRoutes) ||
+                        comparisonValue.EndsWith(EndingsExceptionAles) ||
+                        comparisonValue.EndsWith(EndingsExceptionIles) ||
+                        comparisonValue.EndsWith(EndingsExceptionUles) ||
+                        comparisonValue.EndsWith(EndingsExceptionPlates))
+                        // In this case, just remove the "s".
+                        returnValue = originalValue.Substring(0, comparisonValue.Length - 1);
+                    else
+                    {
+                        if (!originalValue.EndsWith(EndingsSS))
+                        {
+                            // Otherwise, remove the "es".
+                            returnValue = originalValue.Substring(0, comparisonValue.Length - 2);
+                        }
+                        else
+                            returnValue = originalValue;
+                    }
+                }
+                else if (!comparisonValue.EndsWith(EndingsSS) && comparisonValue.EndsWith(EndingsS))
+                    // Standard word - remove the "s"
+                    returnValue = originalValue.Substring(0, comparisonValue.Length - 1);
+            }
+            return returnValue;
+        }
+        /// <summary>
+        /// Modifies the plural word back to its singular form.
+        /// </summary>
+        /// <param name="originalValue">The original value to be modified.</param>
+        /// <returns>
+        /// The English singular form of the provided word.
+        /// </returns>
+        public static string Pluralize(this string originalValue)
+        {
+            string returnValue = originalValue;
+
+            if (!string.IsNullOrEmpty(originalValue))
+            {
+                // Create the comparison string.
+                string comparisonValue = originalValue.ToLower().Trim();
+                originalValue = originalValue.Trim();
+
+                if (comparisonValue.EndsWith('y'))
+                {
+                    // Determine if just adding "s" is needed...
+                    bool endsInS = YWordEndsInS(comparisonValue);
+                    if (endsInS)
+                        returnValue = originalValue + EndingsS;
+                    else
+                    {
+                        // Remove Y and ends with "ies".
+                        returnValue = originalValue.Substring(0, originalValue.Length - 1) + EndingsY;
+                    }
+                }
+                else if (comparisonValue.EndsWith('e'))
+                {
+                    returnValue = originalValue + EndingsS;
+                }
+                else
+                    returnValue = originalValue + EndingsS;
+            }
+            return returnValue;
+        }
+        /// <summary>
+        /// Capitalizes the First Letter of Each Word.
+        /// </summary>
+        /// <param name="s">
+        /// The reference to the string to be modified.
+        /// </param>
+        /// <returns>
+        /// The modified string with each work capitalized.
+        /// </returns>
+        public static string Properize(this string s)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s.ToLower());
+        }
+        /// <summary>
+        /// Surrounds a string with double quotes on each side.
+        /// </summary>
+        /// <param name="originalValue">The original value to be modified.</param>
+        /// <returns>
+        /// The original value with double-quote characters pre-pended and appended to the string.
+        /// </returns>
+        public static string SurroundWithQuotes(this string originalValue)
+        {
+            return $"\"{originalValue}\"";
+        }
+        /// <summary>
+        /// Converts the current string to a <see cref="MemoryStream"/> instance.
+        /// </summary>
+        /// <param name="originalValue">The original value to be modified.</param>
+        /// <returns>
+        /// A <see cref="MemoryStream"/> containing the contents of the current string.
+        /// </returns>
+        public static MemoryStream ToStream(this string originalValue)
+        {
+            return GeneralUtils.ToStream(originalValue);
+        }
+        #endregion
+
+        #region Private Methods / Functions		
+        /// <summary>
+        /// Determines whether the specified the word ending Y is pluralized by adding "s".
+        /// </summary>
+        /// <param name="testString">
+		/// The test string to be checked.
 		/// </param>
-		/// <returns>
-		/// The modified string value.
+        /// <returns>
+		/// <b>true</b> if the word is pluralized by adding "s"; otherwise, the "y" is removed
+		/// and "ies" is added.
 		/// </returns>
-		public static string CleanUpDollarText(this string s)
-		{
-			return s.Replace(DollarSign, string.Empty);
-		}
-		/// <summary>
-		/// Finds the first non-numeric character in the string.
-		/// </summary>
-		/// <param name="original">
-		/// The string instance.
-		/// </param>
-		/// <param name="isFloatingPoint">
-		/// A value indicating whether to allow for a floating-point number.
-		/// </param>
-		/// <returns>
-		/// An integer indicating the first position of a non-numeric character in the string, or 
-		/// -1 if there are no characters to examine or a non-numeric character could not be found.
-		/// </returns>
-		public static int FindFirstNonNumericCharacter(this string original, bool isFloatingPoint)
-		{
-			// Allow one period for floating point numbers.
-			bool dotFound = !isFloatingPoint;
-			int index = 0;
-			int position = -1;
-			int length = original.Length;
+        private static bool YWordEndsInS(string testString)
+        {
+            bool endsInS = false;
 
-			while (index < length && position == -1)
-			{
-				char charToExamine = original[index];
-
-				// If the character is not a digit...
-				if (!char.IsDigit(charToExamine))
-				{
-					// If we have not yet encountered a period, treat the period as a number -
-					// unless <i>isFloatingPoint</i> is <b>false</b>.
-					if (charToExamine == '.' && !dotFound)
-						dotFound = true;
-					else
-						position = index;
-				}
-				index++;
-			};
-
-			return position;
-		}
-		/// <summary>
-		/// Modifies the plural word back to its singular form.
-		/// </summary>
-		/// <param name="originalValue">The original value to be modified.</param>
-		/// <returns>
-		/// The English singular form of the provided word.
-		/// </returns>
-		public static string Singularize(this string originalValue)
-		{
-			string returnValue = originalValue;
-
-			if (!string.IsNullOrEmpty(originalValue))
-			{
-				// Create the comparison string.
-				string comparisonValue = originalValue.ToLower().Trim();
-				originalValue = originalValue.Trim();
-
-				if (comparisonValue.EndsWith(EndingsY))
-				{
-					// Ends with "ies".
-					returnValue = originalValue.Substring(0, comparisonValue.Length - 3) + "y";
-				}
-				else if (comparisonValue.EndsWith(EndingsES))
-				{
-					// Exclude the known exceptions.
-					if (comparisonValue.EndsWith(EndingsExceptionTypes) ||
-						comparisonValue.EndsWith(EndingsExceptionPages) ||
-						comparisonValue.EndsWith(EndingsExceptionRoutes) ||
-						comparisonValue.EndsWith(EndingsExceptionAles) ||
-						comparisonValue.EndsWith(EndingsExceptionIles) ||
-						comparisonValue.EndsWith(EndingsExceptionUles) ||
-						comparisonValue.EndsWith(EndingsExceptionPlates))
-						// In this case, just remove the "s".
-						returnValue = originalValue.Substring(0, comparisonValue.Length - 1);
-					else
-					{
-						if (!originalValue.EndsWith(EndingsSS))
-						{
-							// Otherwise, remove the "es".
-							returnValue = originalValue.Substring(0, comparisonValue.Length - 2);
-						}
-						else
-							returnValue = originalValue;
-					}
-				}
-				else if (!comparisonValue.EndsWith(EndingsSS) && comparisonValue.EndsWith(EndingsS))
-					// Standard word - remove the "s"
-					returnValue = originalValue.Substring(0, comparisonValue.Length - 1);
-			}
-			return returnValue;
-		}
-		/// <summary>
-		/// Modifies the plural word back to its singular form.
-		/// </summary>
-		/// <param name="originalValue">The original value to be modified.</param>
-		/// <returns>
-		/// The English singular form of the provided word.
-		/// </returns>
-		public static string Pluralize(this string originalValue)
-		{
-			string returnValue = originalValue;
-
-			if (!string.IsNullOrEmpty(originalValue))
-			{
-				// Create the comparison string.
-				string comparisonValue = originalValue.ToLower().Trim();
-				originalValue = originalValue.Trim();
-
-				if (comparisonValue.EndsWith("y"))
-				{
-					// Ends with "ies".
-					returnValue = originalValue + EndingsY;
-				}
-				else if (comparisonValue.EndsWith("e"))
-				{
-					returnValue = originalValue + EndingsS;
-				}
-				else
-					returnValue = originalValue + EndingsS;
-			}
-			return returnValue;
-		}
-		/// <summary>
-		/// Capitalizes the First Letter of Each Word.
-		/// </summary>
-		/// <param name="s">
-		/// The reference to the string to be modified.
-		/// </param>
-		/// <returns>
-		/// The modified string with each work capitalized.
-		/// </returns>
-		public static string Properize(this string s)
-		{
-			return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s.ToLower());
-		}
-		/// <summary>
-		/// Surrounds a string with double quotes on each side.
-		/// </summary>
-		/// <param name="originalValue">The original value to be modified.</param>
-		/// <returns>
-		/// The original value with double-quote characters pre-pended and appended to the string.
-		/// </returns>
-		public static string SurroundWithQuotes(this string originalValue)
-		{
-			return $"\"{originalValue}\"";
-		}
-		/// <summary>
-		/// Converts the current string to a <see cref="MemoryStream"/> instance.
-		/// </summary>
-		/// <param name="originalValue">The original value to be modified.</param>
-		/// <returns>
-		/// A <see cref="MemoryStream"/> containing the contents of the current string.
-		/// </returns>
-		public static MemoryStream ToStream(this string originalValue)
-		{
-			return GeneralUtils.ToStream(originalValue);
-		}
-		#endregion
-	}
+            if (testString.Length >= 2)
+            {
+                char vowelCheck = testString[testString.Length - 2];
+                endsInS = (
+                    vowelCheck == 'a' ||
+                    vowelCheck == 'e' ||
+                    vowelCheck == 'i' ||
+                    vowelCheck == 'o' ||
+                    vowelCheck == 'u');
+            }
+            return endsInS;
+        }
+        #endregion
+    }
 }
