577f433803c09967671c0047e9f5cbc94b5e5c85
Sam Jones <samj@clientcommand.com>
2025-07-19 12:55:00 -0400

    Minor update.

--- Shared Code Library/Adaptive.Intelligence.Shared/IO/FileNameRenderer.cs
+++ Shared Code Library/Adaptive.Intelligence.Shared/IO/FileNameRenderer.cs
@@ -2,17 +2,27 @@ namespace Adaptive.Intelligence.Shared.IO;
 
 /// <summary>
 /// Provides static methods and functions for rendering file and path names with
-/// regard to the underlyiong operating system.
+/// regard to the underlying operating system.
 /// </summary>
 public static class FileNameRenderer
 {
     #region Private Static Members
 
+    /// <summary>
+    /// Is Windows Flag.
+    /// </summary>
     private static bool _windows;
+    /// <summary>
+    /// Is Not Windows Flag.
+    /// </summary>
     private static bool _linuxOrMac;
     
     #endregion
 
+    #region Static Constructor
+    /// <summary>
+    /// Performs static initialization.
+    /// </summary>
     static FileNameRenderer()
     {
        _windows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
@@ -24,21 +34,23 @@ public static class FileNameRenderer
            (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(
                System.Runtime.InteropServices.OSPlatform.OSX)));
     }
+    #endregion
 
+    #region Public Static Methods
     /// <summary>
     /// Rewrites the path and file name to the appropriate OS format.
     /// </summary>
     /// <param name="path">
-    /// A string containing the fully-qualified path to modify.
+    /// A string containing the fully qualified path to modify.
     /// </param>
     /// <param name="fileName">
     /// A string containing the file name to modify,
     /// </param>
     /// <returns>
-    /// A string containing the fully-qualified path and file name in the
+    /// A string containing the fully qualified path and file name in the
     /// proper format for the current operating system.
     /// </returns>
-    public static string? RenderFileName(string path, string fileName)
+    public static string RenderFileName(string path, string fileName)
     {
         if (_linuxOrMac)
         {
@@ -47,7 +59,6 @@ public static class FileNameRenderer
         }
 
         return Path.Combine(path, fileName);
-        
     }
     
     /// <summary>
@@ -58,16 +69,26 @@ public static class FileNameRenderer
     /// A string containing the file name to modify,
     /// </param>
     /// <returns>
-    /// A string containing the fully-qualified path and file name in the
+    /// A string containing the fully qualified path and file name in the
     /// proper format for the current operating system.
     /// </returns>
     public static string RenderFileNameInUserPath(string fileName)
     {
        string path = System.Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
        return RenderFileName(path, fileName);
-
     }
 
+    /// <summary>
+    /// Renders the temporary file path with the additional
+    /// subdirectory.
+    /// </summary>
+    /// <param name="path">
+    /// A string containing the sub-directory name to attach
+    /// to the temp file path.
+    /// </param>
+    /// <returns>
+    /// A string containing the rendered path value.
+    /// </returns>
     public static string RenderInTempPath(string path)
     {
         if (_windows)
@@ -79,4 +100,5 @@ public static class FileNameRenderer
                 path);
         }
     }
+    #endregion
 }
